001971b2d5d5433d864d017d8dc01ea12cab7a3c
Ishan Shah <ishan0102@utexas.edu>
2023-11-04 01:52:56 -0400

    Update mkgit

--- functions/mkgit.sh
+++ functions/mkgit.sh
@@ -12,40 +12,95 @@ command_exists() {
     command -v "$1" &> /dev/null
 }
 
+# Function to print a step message
+print_step() {
+    echo "${bold}${blue}=> $1${reset}"
+}
+
+# Function to print success message
+print_success() {
+    echo "${bold}${green}$1${reset}"
+}
+
+# Function to print error message
+print_error() {
+    echo "${bold}${red}$1${reset}"
+}
+
 # Function to initialize the git repository
 initialize_git() {
     if ! command_exists git; then
-        echo "${bold}${red}Git could not be found. Please install it before proceeding.${reset}"
+        print_error "Git could not be found. Please install it before proceeding."
         return 1
     fi
 
+    print_step "Initializing the git repository..."
     git init -b main
     git add .
     git commit -m "Initial commit"
-}
-
-# Function to create a GitHub repository
-create_github_repo() {
-    if ! command_exists gh; then
-        echo "${bold}${red}GitHub CLI could not be found. Please install it before proceeding.${reset}"
-        return 1
-    fi
-
-    if gh repo create "$directory" --source=. --remote=upstream --push --${visibility}; then
-        echo "${bold}${green}Created the repo $directory on GitHub!${reset}"
-    else
-        echo "${bold}${red}Failed to create the GitHub repo. Please check your network connection and try again.${reset}"
-        return 1
+    print_success "Git repository initialized successfully."
+    
+    # Setup Python project if applicable
+    if [[ "$is_python_project" == "yes" ]]; then
+        setup_python_project
     fi
 }
 
 # Function to setup a Python project
 setup_python_project() {
-    echo ".pre-commit-config.yaml" > .gitignore
+    print_step "Setting up the Python project environment..."
+
+    # Create .gitignore
+    echo "venv/" >> .gitignore
+
+    # Create the virtual environment and activate it
     python3 -m venv venv
     source venv/bin/activate
+    print_success "Python virtual environment created and activated."
+
+    # Create .pre-commit-config.yaml with the provided configuration
+    cat <<EOF > .pre-commit-config.yaml
+repos:
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v4.4.0
+    hooks:
+      - id: trailing-whitespace
+      - id: end-of-file-fixer
+  - repo: https://github.com/bwhmather/ssort
+    rev: v0.11.6
+    hooks:
+      - id: ssort
+  - repo: https://github.com/pycqa/isort
+    rev: 5.12.0
+    hooks:
+      - id: isort
+        args: ["--profile", "black", "--filter-files"]
+  - repo: https://github.com/psf/black
+    rev: 23.3.0
+    hooks:
+      - id: black
+        args: ["--line-length", "120"]
+EOF
+
+    # Install pre-commit hooks
     pre-commit install
-    echo "venv/" >> .gitignore
+    print_success "Pre-commit hooks installed."
+}
+
+# Function to create a GitHub repository
+create_github_repo() {
+    if ! command_exists gh; then
+        print_error "GitHub CLI could not be found. Please install it before proceeding."
+        return 1
+    fi
+
+    print_step "Creating the GitHub repository..."
+    if gh repo create "$directory" --source=. --remote=upstream --push --${visibility}; then
+        print_success "Created the repo $directory on GitHub!"
+    else
+        print_error "Failed to create the GitHub repo. Please check your network connection and try again."
+        return 1
+    fi
 }
 
 # Main function to make a git repository
@@ -86,9 +141,6 @@ mkgit() {
     # Check if it's a Python project
     echo -n "${bold}${blue}Is this a Python project? (yes/no): ${reset}"
     read is_python_project
-    if [[ "$is_python_project" == "yes" ]]; then
-        setup_python_project
-    fi
 
     # Ask for vscode settings
     echo -n "${bold}${blue}Would you like to add a .vscode/settings.json file? (yes/no): ${reset}"
